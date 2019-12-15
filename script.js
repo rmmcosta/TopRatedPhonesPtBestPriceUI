8b9657af06c0694cd999391726da293244a89dde
rmmcosta <ricardocosta101085@gmail.com>
2019-12-15 19:34:56 +0100

    trying to fetch the data

---
+++ script.js
@@ -0,0 +1,27 @@
+import { type } from "os";
+
+const proxyurl = "https://cors-anywhere.herokuapp.com";
+const url = "https://topratedphonesptbestprices.herokuapp.com"; // site that doesn’t send Access-Control-*
+const baseApiUrl = proxyurl + '/' + url;
+
+const myHeaders = new Headers();
+
+const myInit = {
+  method: 'GET',
+  headers: myHeaders,
+  mode: 'cors',
+  cache: 'default'
+};
+
+$(() => {
+    console.log('ready to start fetching data');
+    const divPhones = $('#divPhones');
+    fetch(baseApiUrl + '/phones', myInit)
+        .then(html => {
+            divPhones.innerHtml = html;
+            console.log(html);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
